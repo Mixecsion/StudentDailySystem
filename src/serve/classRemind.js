a36d601a410004cdc5448aa69984a2632ebe1569
Lgee <1565692947@qq.com>
2022-06-11 21:55:30 +0800

    冲突合并

--- src/serve/classRemind.js
+++ src/serve/classRemind.js
@@ -1,23 +1,21 @@
-import ClassBox from '../data/db238.json'
-import {GetNowTime} from '../components/timeSetter.vue'
+import ClassBox from "../data/db238.json";
+import { GetNowTime } from "../components/timeSetter.vue";
 
-function classRemind(name){
-    var flag=0;
-    var weekday=GetNowTime()[5]
-    var endTimeHour = GetNowTime()[3]
-    for(var i=0;i<ClassBox.class.length;i++){
-        if(weekday==ClassBox.class[i].date){
-            if(endTimeHour<ClassBox.class[i].endtimeHour){
-                if(flag==0){
-                    flag=1;
-                    return ClassBox.class[i];
-                }
-            }
+function classRemind(name) {
+  var flag = 0;
+  var weekday = GetNowTime()[5];
+  var endTimeHour = GetNowTime()[3];
+  for (var i = 0; i < ClassBox.class.length; i++) {
+    if (weekday == ClassBox.class[i].date) {
+      if (endTimeHour < ClassBox.class[i].endtimeHour) {
+        if (flag == 0) {
+          flag = 1;
+          return ClassBox.class[i];
         }
+      }
     }
-    return -1;
+  }
+  return -1;
 }
 
-export{
-    classRemind
-}
+export { classRemind };
